1457f5fae0d3810567fe79d74012a891a483b76d
Drew Jarrett <drewjarrett@Drews-iMac.local>
2023-03-15 10:49:09 +0000

    drew 9

--- app.js
+++ app.js
@@ -19,8 +19,18 @@ class Hiro extends Component {
 
   handleDrewClick() {
     let scene = document.querySelector('a-scene');
-    alert(scene);
-    alert(this.state.lat + " - " + this.state.lng);
+
+    let model = document.createElement('a-entity');
+    model.setAttribute('gps-entity-place', 'latitude: ' + this.state.lat + '; longitude: ' + this.state.lng + ';');
+    model.setAttribute('scale', '0.5 0.5 0.5');
+    model.setAttribute('rotation', '0 180 0');
+    model.setAttribute('position', '0 0 0');
+    model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
+    model.setAttribute('animation-mixer', '');
+
+    scene.appendChild(model);
+
+    alert('latitude: ' + this.state.lat + '; longitude: ' + this.state.lng + ';');
   }
 
   render() {
@@ -34,8 +44,6 @@ class Hiro extends Component {
           embedded
           arjs='sourceType: webcam; sourceWidth:1280; sourceHeight:960; displayWidth: 1280; displayHeight: 960; debugUIEnabled: false;'>
           <a-camera gps-camera rotation-reader></a-camera>
-
-          <a-entity gltf-model="./assets/magnemite/scene.gltf" rotation="0 180 0" scale="0.15 0.15 0.15" gps-entity-place="longitude: {this.state.lng}; latitude: {this.state.lat};" animation-mixer />
         </a-scene>
       </div>
     );
