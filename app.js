a8bef1f0ccfc9f4e4843388e9fb358a07b74e7d4
Drew Jarrett <drewjarrett@Drews-iMac.local>
2023-03-15 09:54:39 +0000

    drew 5

--- app.js
+++ app.js
@@ -2,16 +2,31 @@ import React, { Component, StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 class Hiro extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {lat : 51.0698916, lng: -1.3118446};
+
+    navigator.geolocation.getCurrentPosition((position) => {
+      alert(position.coords.latitude + " - " + position.coords.longitude);
+    });
+  }
+
   render() {
     return (
-      <a-scene
-       vr-mode-ui="enabled: false"
-       embedded
-       arjs='sourceType: webcam; sourceWidth:1280; sourceHeight:960; displayWidth: 1280; displayHeight: 960; debugUIEnabled: false;'>
-       <a-camera gps-camera rotation-reader></a-camera>
+      <div>
+        <div class="aSceneControls">
+          DREW
+        </div>
+        <a-scene
+          vr-mode-ui="enabled: false"
+          embedded
+          arjs='sourceType: webcam; sourceWidth:1280; sourceHeight:960; displayWidth: 1280; displayHeight: 960; debugUIEnabled: false;'>
+          <a-camera gps-camera rotation-reader></a-camera>
 
-       <a-entity gltf-model="test.jpg" rotation="0 180 0" scale="0.15 0.15 0.15" gps-entity-place="longitude: -1.3118446; latitude: 51.0698916;" animation-mixer />
-      </a-scene>
+          <a-entity gltf-model="test.jpg" rotation="0 180 0" scale="0.15 0.15 0.15" gps-entity-place="longitude: -1.3118446; latitude: 51.0698916;" animation-mixer />
+        </a-scene>
+      </div>
     );
   }
 }
@@ -26,10 +41,6 @@ class App extends Component {
   }
 
   handleStartClick() {
-    navigator.geolocation.getCurrentPosition((position) => {
-      alert(position.coords.latitude + " - " + position.coords.longitude);
-    });
-
     this.setState({startVr : true });
   }
 
