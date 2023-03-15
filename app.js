fafbef00a6dcfad52397478a9fe0c37fcb3e2395
Drew Jarrett <drewjarrett@Drews-iMac.local>
2023-03-15 10:02:54 +0000

    drew 7

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ class Hiro extends Component {
         lat : position.coords.latitude,
         lng : position.coords.longitude
       });
-      alert(position.coords.latitude + " - " + position.coords.longitude);
     });
 
     this.handleDrewClick = this.handleDrewClick.bind(this);
@@ -34,7 +33,7 @@ class Hiro extends Component {
           arjs='sourceType: webcam; sourceWidth:1280; sourceHeight:960; displayWidth: 1280; displayHeight: 960; debugUIEnabled: false;'>
           <a-camera gps-camera rotation-reader></a-camera>
 
-          <a-entity gltf-model="test.jpg" rotation="0 180 0" scale="0.15 0.15 0.15" gps-entity-place="longitude: -1.3118446; latitude: 51.0698916;" animation-mixer />
+          <a-entity gltf-model="test.jpg" rotation="0 180 0" scale="0.15 0.15 0.15" gps-entity-place="longitude: {this.state.lng}; latitude: {this.state.lat};" animation-mixer />
         </a-scene>
       </div>
     );
