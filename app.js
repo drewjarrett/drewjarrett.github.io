ad6b03ae32e98d9261ebaadaf1baf45546759889
Drew Jarrett <drewjarrett@Drews-iMac.local>
2023-03-15 10:21:40 +0000

    drew 7

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ class Hiro extends Component {
           arjs='sourceType: webcam; sourceWidth:1280; sourceHeight:960; displayWidth: 1280; displayHeight: 960; debugUIEnabled: false;'>
           <a-camera gps-camera rotation-reader></a-camera>
 
-          <a-entity gltf-model="test.jpg" rotation="0 180 0" scale="0.15 0.15 0.15" gps-entity-place="longitude: {this.state.lng}; latitude: {this.state.lat};" animation-mixer />
+          <a-entity gltf-model="./assets/magnemite/scene.gltf" rotation="0 180 0" scale="0.15 0.15 0.15" gps-entity-place="longitude: {this.state.lng}; latitude: {this.state.lat};" animation-mixer />
         </a-scene>
       </div>
     );
